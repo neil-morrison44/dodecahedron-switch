0c9bbbf7c915dc29457baf5d1f23b104b308b142
Neil Morrison <neil.morrison44@googlemail.com>
2020-12-30 21:43:19 +0000

    now sends the right stuff to homebridge-mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 #define WIFI_TIMEOUT_MS 30000
 
 #define TIME_TO_SLEEP 60 * 60 * 24
-#define uS_TO_S_FACTOR 1000000
+#define uS_TO_S_FACTOR 1000000LL
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -23,6 +23,7 @@ int value = 0;
 Adafruit_LSM6DS33 lsm6ds33;
 
 RTC_DATA_ATTR int bootCount = 0;
+// RTC_DATA_ATTR int lastPrediction = -1;
 
 bool wifiConnected = false;
 bool messagesSent = false;
@@ -77,6 +78,28 @@ void connectToWifi()
   WiFi.begin(SSID, PASSWORD);
 }
 
+void registerDevice()
+{
+  Serial.println("Registering device...");
+
+  // sprintf(msg, "{\"name\": \"dodecahedron\"}");
+  // client.publish("homebridge/to/remove", msg);
+
+  sprintf(msg, "{\"name\": \"dodecahedron\",\"service_name\": \"face 0\", \"service\": \"Switch\" }");
+  client.publish("homebridge/to/add", msg);
+
+  for (int i = 1; i < 12; i++)
+  {
+    sprintf(msg, "{\"name\": \"dodecahedron\",  \"service_name\": \"face %d\", \"service\": \"Switch\" }", i);
+    client.publish("homebridge/to/add/service", msg);
+  }
+
+  sprintf(msg, "{\"name\": \"dodecahedron\",  \"service_name\": \"battery\", \"service\": \"BatteryService\" }");
+  client.publish("homebridge/to/add/service", msg);
+
+  // delay(100);
+}
+
 void sendMessage()
 {
   Serial.println("Sending message");
@@ -87,13 +110,18 @@ void sendMessage()
   lsm6ds33.getEvent(&accel, &gyro, &temp);
 
   int prediction = predict(accel.acceleration.x, accel.acceleration.y, accel.acceleration.z, gyro.gyro.x, gyro.gyro.y, gyro.gyro.z);
-  sprintf(msg, "prediction: %d", prediction);
-  client.publish("test/predict", msg);
-  // take readings
-  // make prediction
+  // if (prediction == lastPrediction)
+  // {
+  //   Serial.println("No change in prediction");
+  //   return;
+  // }
+  for (int i = 0; i < 12; i++)
+  {
+    sprintf(msg, "{\"name\": \"dodecahedron\",  \"service_name\": \"face %d\", \"characteristic\": \"On\", \"value\": %d }", i, (i == prediction));
+    client.publish("homebridge/to/set", msg);
+  }
 
-  // set all (other) switches to off
-  // set predicted switch to on
+  // lastPrediction = prediction;
 }
 
 void sendBatteryMessage()
@@ -101,8 +129,17 @@ void sendBatteryMessage()
   Serial.println("Sending battery message");
   int batteryPercentage = getBatteryPercentage();
 
-  sprintf(msg, "Battery: %d%%", batteryPercentage);
-  client.publish("test/battery", msg);
+  // sprintf(msg, "Battery: %d%%", batteryPercentage);
+  // client.publish("test/battery", msg);
+
+  // topic: homebridge/to/set
+  // payload: {"name": "dodecahedron", "service_name": "battery", "characteristic": "BatteryLevel", "value": true}
+
+  sprintf(msg, "{\"name\": \"dodecahedron\",  \"service_name\": \"battery\", \"characteristic\": \"BatteryLevel\", \"value\": %d }", batteryPercentage);
+  client.publish("homebridge/to/set", msg);
+
+  sprintf(msg, "{\"name\": \"dodecahedron\",  \"service_name\": \"battery\", \"characteristic\": \"StatusLowBattery\", \"value\": %d }", batteryPercentage < 20);
+  client.publish("homebridge/to/set", msg);
 }
 
 void disconnectEverything()
@@ -141,6 +178,7 @@ void onWake()
     break;
   default:
     Serial.printf("Wakeup was not caused by deep sleep: %d\n", wakeup_reason);
+    registerDevice();
     delay(1000);
     gotoSleep();
     break;
